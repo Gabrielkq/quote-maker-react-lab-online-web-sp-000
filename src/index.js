e4f39aed0e0dcb496c5046d215b4278288ca43a6
Gabrielkq <gabrielkutik@yahoo.com>
2020-11-10 11:26:15 -0500

    commit tree 7b79a7aa37b7b1aadb9a05f1f2ee8ab7c86dbb43

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux'
+//import { createStore } from 'redux'
 import rootReducer from './reducers/index'
+import {configureStore} from './store.js'
 
-let store = createStore(rootReducer)
+let store = configureStore(rootReducer)
 
 ReactDOM.render(
   <Provider store={store}>
